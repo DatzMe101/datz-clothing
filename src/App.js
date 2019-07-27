317a50a108a80119df7e722e96668d49466f86c4
Elmer Datolayta <e.datolaytz@gmail.com>
2019-07-27 23:45:35 +1000

    Add header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shopPage.component";
+import Header from "./components/header/header.component";
 import "./App.css";
-
-const HatsPage = () => {
-  return <h1>Hats Page</h1>;
-};
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
         <Route exact path="/" component={HomePage} />
       </Switch>
     </div>
---
+++ src/pages/shop/shopPage.component.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import SHOP_DATA from "./shopPage.data";
+import "./shop.styles.scss";
+
+class Shop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: [...SHOP_DATA]
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div>
+        {collections.map(collection => {
+          return <CollectionPreview {...collection} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default Shop;
