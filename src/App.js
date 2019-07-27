910f924c888968710dc0dc9451252ce3afc9213a
Elmer Datolayta <e.datolaytz@gmail.com>
2019-07-27 22:56:44 +1000

    update app with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
+
+const HatsPage = () => {
+  return <h1>Hats Page</h1>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/shop/hats" component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
