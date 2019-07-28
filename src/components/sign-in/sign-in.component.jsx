de13f7c0d2b835b7a8f07ff1492361d75d860b03
Elmer Datolayta <e.datolaytz@gmail.com>
2019-07-28 20:34:32 +1000

    Update sign in with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
 class SignIn extends Component {
   constructor(props) {
@@ -11,9 +11,15 @@ class SignIn extends Component {
       password: ""
     };
   }
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      alert("Invalid username or password");
+    }
   };
   handleChange = event => {
     const { value, name } = event.currentTarget;
